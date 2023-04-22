7c93c224573f8e1af0979d9655b26490872bd8e0
Panagiotis Mandros <pmandros@gmail.com>
2023-04-22 22:37:10 +0000

    requirements

--- conda_install.sh
+++ conda_install.sh
@@ -4,5 +4,3 @@ conda install -c conda-forge umap-learn
 conda install openblas numpy scipy scikit-learn numexpr
 conda install -c stellargraph stellargraph
 conda install -c conda-forge nbformat
-pip install git+https://github.com/djcunningham0/rankaggregation.git
-pip install git+https://github.com/iggallagher/Spectral-Embedding
